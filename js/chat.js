1e9e98d4f94cefd752ca342286f68a2291698012
unknown <chm2920@163.com>
2011-12-27 10:45:27 +0800

    'reinit'

--- js/chat.js
+++
@@ -1,8 +0,0 @@
-
-var chat = {
-	init: function(){
-		$('#send').click(function(){
-			$('#chatList').append('<p><span>Me:</span>' + $('#speak').val() + '</p>');
-		});
-	}
-};
--- js/util.js
+++
@@ -1,8 +0,0 @@
-
-var Util = {
-	log: function(){
-		
-	}
-};
-
-
